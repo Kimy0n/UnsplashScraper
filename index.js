82e1df465976f5044433622ac93551f6c7ceb46f
Kimy0n <84599859+Kimy0n@users.noreply.github.com>
2022-03-18 21:37:12 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const PORT = process.env.PORT || 8080;
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors())
 
 app.get('/image/:string', (req, res) => {
     const path = `https://unsplash.com/s/photos/${req.params.string}`;
