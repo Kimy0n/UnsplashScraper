e3221dd1ff3678e5d84b4b19cf25a52d0688c90f
Kimy0n <84599859+Kimy0n@users.noreply.github.com>
2022-03-18 21:16:35 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const PORT = process.envçPORT || 8080;
+
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const app = express();
+
+app.get('/image/:string', (req, res) => {
+    const path = `https://unsplash.com/s/photos/${req.params.string}`;
+    var imageList = [];
+
+    axios.get(path).then((response) => {
+        const $ = cheerio.load(response.data);
+        console.log(path)
+        var id = 0
+        $(".YVj9w").each((i, el) => {
+            var src = $(el).attr('src')
+            if (src != null) {
+                imageList.push({
+                    "id": id,
+                    "url": src
+                })
+                id++
+            }
+        })
+    })
+
+    setTimeout(() => res.json(imageList), 2000)
+
+
+})
+
+app.listen(PORT, () => console.log(`Server running on Port: ${PORT}`))
